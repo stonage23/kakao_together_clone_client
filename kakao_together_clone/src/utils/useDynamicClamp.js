603c0fae20c9f81361b9b1c897760c236d7c91e3
stonage23 <stonage23@gmail.com>
2024-06-13 09:01:25 +0900

    utils/useDynamicClamp(수정) - scrollHeight으로 계산하는 것에서 clientHeight으로 변경

--- kakao_together_clone/src/utils/useDynamicClamp.js
+++ kakao_together_clone/src/utils/useDynamicClamp.js
@@ -1,25 +1,34 @@
 import { useState, useEffect, useRef } from 'react';
 
-const useDynamicClamp = (title, content) => {
-  const [lineClamp, setLineClamp] = useState(5);
-  const titleRef = useRef(null);
-
+/**
+ *
+ * @param {string} state line clamp에 영향을 주는 기준 변수(또는 상태)
+ * @param {number} short 짧은 경우의 줄
+ * @param {number} long 긴 경우의 줄
+ * @param {number} [targetLine=1] line clamp에 영향을 주는 문자열의 기준 
+ * @returns
+ */
+const useDynamicClamp = (state, short, long, targetLine = 1) => {
+  const [lineClamp, setLineClamp] = useState(0);
+  const targetRef = useRef(null);
 
   useEffect(() => {
     const applyDynamicClamp = () => {
-      const titleElement = titleRef.current;
-      if (!titleElement) return;
+      const targetElement = targetRef.current;
+      if (!targetElement) return;
 
-      // Ensure styles are applied correctly and fully loaded
       setTimeout(() => {
-        const titleLineHeight = parseFloat(getComputedStyle(titleElement).lineHeight);
-        const titleHeight = titleElement.scrollHeight;
+        const titleLineHeight = parseFloat(
+          getComputedStyle(targetElement).lineHeight
+        );
+        if (titleLineHeight === 0) return;
+        const titleHeight = targetElement.clientHeight;
         const linesInTitle = Math.round(titleHeight / titleLineHeight);
 
-        if (linesInTitle > 1) {
-          setLineClamp(4); // 타이틀이 2줄이면 본문 4줄로 제한
+        if (linesInTitle > targetLine) {
+          setLineClamp(short);
         } else {
-          setLineClamp(5); // 타이틀이 1줄이면 본문 5줄로 제한
+          setLineClamp(long);
         }
       }, 0);
     };
@@ -30,9 +39,9 @@ const useDynamicClamp = (title, content) => {
     return () => {
       window.removeEventListener('resize', applyDynamicClamp);
     };
-  }, [title, content]);
+  }, [state]);
 
-  return { lineClamp, titleRef };
+  return { lineClamp, targetRef };
 };
 
 export default useDynamicClamp;
